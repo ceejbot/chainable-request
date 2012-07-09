d32dbd613fad2264362c275ac2df42158ba78492
C J Silverio <ceejceej@gmail.com>
2012-07-08 17:36:45 -0700

    Cleanup & modernization.
    My first-ever node project has some noob dust on it still.

--- chainable-request.js
+++ chainable-request.js
@@ -1,21 +1,21 @@
-var chainableRequest = ((typeof module !== 'undefined') && module.exports) || {};
+(function() {
+"use strict";
 
-(function(exports)
-{
+var events      = require('events'),
+    querystring = require('querystring'),
+    util        = require('util')
+    ;
 
-var events      = require('events');
-    querystring = require('querystring');
-
-function chainableRequest(options)
+function ChainableRequest(options)
 {
 	events.EventEmitter.call(this);
-	
+
 	var host = 'localhost';
 	if (options.hostname !== undefined)
 		host = options.hostname;
 	else if (options.host !== undefined)
 		host = options.host;
-	
+
 	this.parameters = {
 		'method': 'GET',
 		'headers': { },
@@ -23,69 +23,59 @@ function chainableRequest(options)
 		'port': (options.port === undefined) ? 80 : options.port,
 		'path': (options.path === undefined) ? '/' : options.path
 	};
+
 	var protocol = 'http';
-	// TODO enforce protocol as http or https
-	if (options.protocol && (options.protocol[options.protocol.length - 1] == ':'))
-		protocol = options.protocol.slice(0, -1);
+	if (options.protocol)
+		protocol = options.protocol;
+	protocol = protocol.replace(/:$/, '');
 	this.protocol = require(protocol);
 
 	this.querystring = false;
 }
-chainableRequest.super_ = events.EventEmitter;
-chainableRequest.prototype = Object.create(
-	events.EventEmitter.prototype,
-	{
-		constructor:
-		{
-			value: chainableRequest,
-			enumerable: false
-		}
-	}
-);
-exports.chainableRequest = chainableRequest;
+util.inherits(ChainableRequest, events.EventEmitter);
 
-chainableRequest.prototype.host = function(host)
+ChainableRequest.prototype.host = function(host)
 {
 	this.parameters.host = host;
 	return this;
 };
 
-chainableRequest.prototype.port = function(port)
+ChainableRequest.prototype.port = function(port)
 {
 	this.parameters.port = port;
 	return this;
 };
 
-chainableRequest.prototype.protocol = function(proto)
+ChainableRequest.prototype.protocol = function(proto)
 {
 	// TODO restrict protocols to one of http, https
-	if (proto[proto.length - 1] == ':')
+	if (proto[proto.length - 1] === ':')
 		proto = proto.slice(0, -1);
 	this.protocol = require(proto);
 	return this;
 };
 
-chainableRequest.prototype.headers = function(headers)
+ChainableRequest.prototype.headers = function(headers)
 {
 	__extend(this.parameters.headers, headers);
 	return this;
 };
 
-chainableRequest.prototype.content_type = function(type)
+ChainableRequest.prototype.content_type = function(type)
 {
 	__extend(this.parameters.headers, {'Content-Type': type});
 	return this;
 };
 
-chainableRequest.prototype.query = function(params)
+ChainableRequest.prototype.query = function(params)
 {
 	this.querystring = querystring.stringify(params);
 	return this;
 };
 
-chainableRequest.prototype.body = function(body)
+ChainableRequest.prototype.body = function(body)
 {
-	if (typeof body == 'object')
+	if (typeof body === 'object')
 	{
 		this.payload = querystring.stringify(body);
 		this.content_type('application/x-www-form-urlencoded');
@@ -95,7 +85,7 @@ chainableRequest.prototype.body = function(body)
 	return this;
 };
 
-chainableRequest.prototype.basic_auth = function(user, password)
+ChainableRequest.prototype.basic_auth = function(user, password)
 {
 	// TODO test-- completely unexercised code.
 	var enc = new Buffer(user+':'+password).toString('base64');
@@ -103,18 +93,18 @@ chainableRequest.prototype.basic_auth = function(user, password)
 	return this;
 };
 
-chainableRequest.prototype.get = function(path)
+ChainableRequest.prototype.get = function(path)
 {
 	if (path !== undefined)
 		this.parameters.path = path;
 	if (this.querystring)
-		this.parameters.path = this.parameters.path + '?' + this.querystring;		
+		this.parameters.path = this.parameters.path + '?' + this.querystring;
 
 	this.execute();
 	return this;
 };
 
-chainableRequest.prototype.post = function(path)
+ChainableRequest.prototype.post = function(path)
 {
 	this.parameters.method = 'POST';
 	if (path !== undefined)
@@ -124,7 +114,7 @@ chainableRequest.prototype.post = function(path)
 	return this;
 };
 
-chainableRequest.prototype.put = function(path)
+ChainableRequest.prototype.put = function(path)
 {
 	this.parameters.method = 'PUT';
 	if (path !== undefined)
@@ -134,7 +124,7 @@ chainableRequest.prototype.put = function(path)
 	return this;
 };
 
-chainableRequest.prototype.delete = function(path)
+ChainableRequest.prototype.delete = function(path)
 {
 	this.parameters.method = 'DELETE';
 	if (path !== undefined)
@@ -144,28 +134,25 @@ chainableRequest.prototype.delete = function(path)
 	return this;
 };
 
-chainableRequest.prototype.execute = function()
+ChainableRequest.prototype.execute = function()
 {
 	var self = this;
-	
+
 	if (self.payload === undefined)
 		__extend(this.parameters.headers, {'Content-Length': 0});
 	else
 		__extend(this.parameters.headers, {'Content-Length': Buffer.byteLength(self.payload)});
-	
+
 	var request = self.protocol.request(self.parameters, function(response)
 	{
-		if ((response.headers['transfer-encoding'] == 'chunked'))
-			self.handleChunkedResponse(response);
-		else
-			self.handleResponse(response);	
+		self.handleResponse(response);
 	});
-	
+
 	request.on('error', function(err)
 	{
 		self.emit('error', err);
 	});
-	
+
 	if (this.payload !== undefined)
 		request.write(this.payload);
 
@@ -180,7 +167,8 @@ function parseMimeType(header)
 	if (pieces.length > 1)
 		result.charset = pieces[1].replace('charset=', '').trim();
 	else
-		result.charset = 'ascii';
+		result.charset = 'utf8';
+
 	return result;
 }
 
@@ -193,15 +181,15 @@ function processResponse(mimetype, data)
 	{
 		case 'text/plain':
 		case 'text/html':
-			result = data.toString(type.charset);
+			result = data.toString('utf8');
 			break;
 
 		case 'application/x-www-form-urlencoded':
-			result = querystring.parse(data.toString(type.charset))
+			result = querystring.parse(data.toString('utf8'));
 			break;
 
 		case 'application/json':
-			result = JSON.parse(data.toString(type.charset))
+			result = JSON.parse(data.toString('utf8'));
 			break;
 
 		case 'image/png':
@@ -213,55 +201,25 @@ function processResponse(mimetype, data)
 
 		default:
 			console.log(type);
-			break;			
+			break;
 	}
-	
+
 	return result;
 }
 
-chainableRequest.prototype.handleResponse = function(response)
-{
-	var self = this;
-
-	var size = parseInt(response.headers['content-length']);
-	var buffptr = 0;
-	var data = new Buffer(size);
-
-	response.on('data', function(chunk)
-	{
-		chunk.copy(data, buffptr);
-		buffptr += chunk.length;
-	});
-
-	var complete = function()
-	{
-		self.emit('reply', response, processResponse(response.headers['content-type'], data));
-	};
-
-	response.on('end', function()
-	{
-		complete();
-	});
-
-	response.on('close', function()
-	{
-		complete();
-	});
-}
-
-chainableRequest.prototype.handleChunkedResponse = function(response)
+ChainableRequest.prototype.handleResponse = function(response)
 {
 	var self = this;
 	var data = null;
-	
+
 	var complete = function()
-	{		
+	{
 		self.emit('reply', response, processResponse(response.headers['content-type'], data));
 	};
 
 	response.on('data', function(chunk)
 	{
-		if (data == null)
+		if (!data)
 			data = chunk;
 		else
 		{
@@ -272,27 +230,18 @@ chainableRequest.prototype.handleChunkedResponse = function(response)
 		}
 	});
 
-	response.on('end', function()
-	{
-		complete();
-	});
-
-	response.on('close', function()
-	{
-		complete();
-	});
-}
+	response.on('end', complete);
+	response.on('close', complete);
+};
 
 function __extend(destination, source)
 {
-	for (var property in source)
-	{
-		if (source.hasOwnProperty(property))
-			destination[property] = source[property];
-	}
+	var keys = Object.keys(source);
+	for (var i = 0; i < keys.length; i++)
+		destination[keys[i]] = source[keys[i]];
 	return destination;
 }
 
-exports.chainableRequest = chainableRequest;
+exports.ChainableRequest = ChainableRequest;
 
-})(chainableRequest);
+}.call(this));
